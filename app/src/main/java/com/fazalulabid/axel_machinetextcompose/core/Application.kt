97a8dec67217ed99eccda1fd20a169cd58544cc3
Fazalul Abid <fazalulabid007@gmail.com>
2024-11-10 15:52:28 +0530

    Initial Commit

---
+++ app/src/main/java/com/fazalulabid/axel_machinetextcompose/core/Application.kt
@@ -0,0 +1,8 @@
+package com.fazalulabid.axel_machinetextcompose.core
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application : Application() {
+}
